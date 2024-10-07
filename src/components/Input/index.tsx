7f32ffb89979ba7510b0690d38f7b44210d2757c
DESKTOP-NC3R0V8\thanh <thanhbtse02590@gmail.com>
2024-10-08 01:06:20 +0700

    test

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -11,56 +11,106 @@ export interface InputProps {
   onSelectItem: (item: string) => void;
 }
 
-const Input = ({ placeholder, onSelectItem}: InputProps) => {
+export interface InputStates {
+  isInitital?: boolean;
+  isFetching?: boolean;
+  errorMessage?: string;
+  searchData?: string[];
+}
+
+const Input = ({ placeholder, onSelectItem }: InputProps) => {
   // DO NOT remove this log
   console.log("input re-render");
 
-  const [tempValue, setTempValue] = useState<string>("");
-  const [isSearching, setIsSearching] = useState<boolean>(false);
-  const [searchData, setSearchData] = useState<string[]>([]);
+  const currentRenderTime = useRef<number>(0);
+  const [inputState, setInputState] = useState<InputStates | null>(null);
 
-  const handleCallAPI = useCallback(async (searchTerm: string) => {
-    if (!searchTerm) return;
-    setIsSearching(true);
-    try {
-      const response = await fetchData(searchTerm);
-      console.log(response);
-      setSearchData(response);
-    } catch (error) {
-      console.log("error when searching", error);
-    } finally {
-      setIsSearching(false);
+  const handleCallAPI = debounce(async (e: ChangeEvent<HTMLInputElement>) => {
+    const searchTerm = e.target.value;
+    if (!searchTerm) {
+      setInputState(null);
+      return;
     }
-  }, []);
 
-  const renderResultBlock = (searchData: string[]) => {
+    const renderTime = currentRenderTime.current + 1;
+    currentRenderTime.current = renderTime;
+    if (!inputState?.isFetching) {
+      setInputState({ isFetching: true });
+    }
+    if (currentRenderTime.current === renderTime && inputState?.isFetching) {
+      try {
+        const response = await fetchData(searchTerm);
+
+        console.log(response);
+        setInputState({
+          searchData: response,
+          errorMessage: "",
+          isFetching: false,
+        });
+      } catch (error) {
+        console.log("error when searching", error);
+        setInputState({
+          errorMessage: `error when searching ${error}`,
+          searchData: [],
+          isFetching: false,
+        });
+      }
+    }
+  }, 100);
+
+  const renderHasResultBlock = () => {
     return (
-      <>
-        {!!searchData?.length ? (
-          <div className={"input-search-component__result-block--has-data"}>
-            {searchData.map((item, index) => {
-              return (
-                <div
-                  key={`search-result-${index}`}
-                  className={"input-search-component__result-item"}
-                  onClick={(
-                    e: React.MouseEvent<HTMLDivElement, MouseEvent>
-                  ) => {
-                    e.stopPropagation;
-                    onSelectItem(item);
-                  }}
-                >
-                  {item}
-                </div>
-              );
-            })}
-          </div>
-        ) : (
-          <div className={"input-search-component__result-block--no-data"}>
-            No data matching your search. Pleas try again!
-          </div>
+      <div className={"input-search-component__result-block--has-data"}>
+        {inputState?.searchData?.map((item, index) => {
+          return (
+            <div
+              key={`search-result-${index}`}
+              className={"input-search-component__result-item"}
+              onClick={() => {
+                onSelectItem(item);
+              }}
+            >
+              {item}
+            </div>
+          );
+        })}
+      </div>
+    );
+  };
+
+  const renderBlankResultBlock = () => {
+    return (
+      !inputState?.errorMessage &&
+      !inputState?.searchData?.length && (
+        <div className={"input-search-component__result-block--no-data"}>
+          "No data matching your search. Pleas try again! "
+        </div>
+      )
+    );
+  };
+
+  const renderErrorBlock = () => {
+    return (
+      inputState?.errorMessage && (
+        <div className={"input-search-component__result-block--no-data"}>
+          {inputState?.errorMessage}
+        </div>
+      )
+    );
+  };
+
+  const renderResultBlock = () => {
+    return (
+      <div className={"input-search-component__result-block"}>
+        {inputState?.isFetching && <Loader />}
+        {!inputState?.isFetching && (
+          <>
+            {renderHasResultBlock}
+            {renderBlankResultBlock}
+            {renderErrorBlock}
+          </>
         )}
-      </>
+      </div>
     );
   };
 
@@ -69,19 +119,10 @@ const Input = ({ placeholder, onSelectItem}: InputProps) => {
     <div className={"input-search-component"}>
       <input
         placeholder={placeholder}
-        onChange={debounce((e: ChangeEvent<HTMLInputElement>) => {
-          e.preventDefault();
-          const searchTerm = e.target?.value;
-          setTempValue(searchTerm);
-          handleCallAPI(searchTerm || "");
-        }, 300)}
+        onChange={handleCallAPI}
         className={"input-search-component__input-search"}
       />
-      {!!tempValue && (
-        <div className={"input-search-component__result-block"}>
-          {isSearching ? <Loader /> : renderResultBlock(searchData)}
-        </div>
-      )}
+      {renderResultBlock()}
     </div>
   );
   // Your code end here
